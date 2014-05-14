a7eb31d2e683c0e8df32294526835c00c3a2e768
gcummins <github@8people.com>
2014-05-14 13:52:50 -0500

    Update connect.php

--- usr/local/dataloader/connect.php
+++ usr/local/dataloader/connect.php
@@ -2,9 +2,9 @@
 
 /*
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '1yaaya5');
-define('DB_NAME', 'datafeeds2');
+define('DB_USER', 'mysql_user');
+define('DB_PASSWORD', 'pass');
+define('DB_NAME', 'your_db');
 */
 
 if (false !== ($link = mysql_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD)))
